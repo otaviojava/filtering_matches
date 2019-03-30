3bc4cabe9e98a118bb6a3c20714691b032247c4a
Otavio Santana <otaviopolianasantana@gmail.com>
2019-03-30 19:36:43 -0300

    creates AppConfig to spring tests

--- src/test/java/br/com/otavio/personality/AppConfig.java
+++ src/test/java/br/com/otavio/personality/AppConfig.java
@@ -1,4 +1,9 @@
 package br.com.otavio.personality;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.otavio.personality")
 public class AppConfig {
 }
--- src/test/java/br/com/otavio/personality/MigrationTest.java
+++ src/test/java/br/com/otavio/personality/MigrationTest.java
@@ -1,15 +1,18 @@
 package br.com.otavio.personality;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = AppConfig.class)
 class MigrationTest {
 
     @Autowired
