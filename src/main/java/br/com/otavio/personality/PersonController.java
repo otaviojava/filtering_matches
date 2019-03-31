e797c7dbf475d7e362f3dbef597d26bb2e80dfce
Otavio Santana <otaviopolianasantana@gmail.com>
2019-03-31 12:13:38 -0300

    creates and improves controller to person

--- src/main/java/br/com/otavio/personality/PersonController.java
+++ src/main/java/br/com/otavio/personality/PersonController.java
@@ -1,4 +1,36 @@
 package br.com.otavio.personality;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.Point;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
+
+@RestController
 public class PersonController {
+
+    @Autowired
+    private FilterConverter converter;
+
+    @Autowired
+    private PersonSearch personSearch;
+
+    @Autowired
+    private UserPointConverter userPointConverter;
+
+    @GetMapping(value = "/people", produces = "application/json")
+    public List<PersonDTO> greeting(@RequestParam Map<String, String> params,
+                                    @RequestHeader Map<String, String> header) {
+
+        Point point = userPointConverter.apply(header);
+        Filter filter = converter.apply(params);
+        List<Person> search = personSearch.search(filter, point);
+        return search.stream().map(PersonDTO::of).collect(toList());
+    }
 }
--- src/test/java/br/com/otavio/personality/PersonControllerTest.java
+++ src/test/java/br/com/otavio/personality/PersonControllerTest.java
@@ -1,5 +1,182 @@
-import static org.junit.jupiter.api.Assertions.*;
+package br.com.otavio.personality;
 
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class PersonControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private Migration migration;
+
+
+    @BeforeEach
+    public void setUp() {
+        migration.migrate();
+
+    }
+
+
+    @Test
+    public void shouldReturnErrorWhenUserInformationIsMissing() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(getURL())
+                .queryParam("photo", false)
+                .queryParam("contact", false)
+                .queryParam("favourite", false)
+                .queryParam("lowerBound", true)
+                .queryParam("score", "invalid");
+
+        ResponseEntity<?> entity = restTemplate.exchange(
+                builder.toUriString(),
+                GET,
+                new HttpEntity<>(headers),
+                (Class<Object>) null);
+
+        assertEquals(UNAUTHORIZED, entity.getStatusCode());
+    }
+
+    @Test
+    public void shouldReturnBadRequestWhenThereIsInvalidValue() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        headers.set("lat", "50.614429");
+        headers.set("lon", "-2.457621");
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(getURL())
+                .queryParam("photo", false)
+                .queryParam("contact", false)
+                .queryParam("favourite", false)
+                .queryParam("lowerBound", true)
+                .queryParam("score", "invalid");
+
+        ResponseEntity<?> entity = restTemplate.exchange(
+                builder.toUriString(),
+                GET,
+                new HttpEntity<>(headers),
+                (Class<Object>) null);
+
+        assertEquals(BAD_REQUEST, entity.getStatusCode());
+    }
+
+    @Test
+    public void shouldReturnBadRequestWhenThereIsOutRangedValues() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        headers.set("lat", "50.614429");
+        headers.set("lon", "-2.457621");
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(getURL())
+                .queryParam("photo", false)
+                .queryParam("contact", false)
+                .queryParam("favourite", false)
+                .queryParam("lowerBound", true)
+                .queryParam("score", 150);
+
+        ResponseEntity<?> entity = restTemplate.exchange(
+                builder.toUriString(),
+                GET,
+                new HttpEntity<>(headers),
+                (Class<Object>) null);
+
+        assertEquals(BAD_REQUEST, entity.getStatusCode());
+    }
+
+    @Test
+    public void shouldRequestFilterNotPhoto() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        headers.set("lat", "50.614429");
+        headers.set("lon", "-2.457621");
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(getURL())
+                .queryParam("photo", false)
+                .queryParam("contact", false)
+                .queryParam("favourite", false)
+                .queryParam("lowerBound", true);
+
+        ResponseEntity<List<PersonDTO>> entity = restTemplate.exchange(
+                builder.toUriString(),
+                GET,
+                new HttpEntity<>(headers),
+                new ParameterizedTypeReference<List<PersonDTO>>() {
+                });
+
+        assertEquals(OK, entity.getStatusCode());
+        List<PersonDTO> body = entity.getBody();
+        Assertions.assertNotNull(body);
+
+        List<String> names = body.stream().map(PersonDTO::toPerson).map(Person::getName).collect(Collectors.toList());
+        assertThat(names, Matchers.contains("Katie"));
+
+    }
+
+    @Test
+    public void shouldRequestFilterPhotoContactsAndAgeHeightScore() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        headers.set("lat", "51.509865");
+        headers.set("lon", "-0.118092");
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(getURL())
+                .queryParam("photo", true)
+                .queryParam("contact", true)
+                .queryParam("favourite", false)
+                .queryParam("lowerBound", true)
+                .queryParam("age", 40)
+                .queryParam("height", 144)
+                .queryParam("score", 50D);
+
+        ResponseEntity<List<PersonDTO>> entity = restTemplate.exchange(
+                builder.toUriString(),
+                GET,
+                new HttpEntity<>(headers),
+                new ParameterizedTypeReference<List<PersonDTO>>() {
+                });
+
+        assertEquals(OK, entity.getStatusCode());
+        List<PersonDTO> body = entity.getBody();
+        Assertions.assertNotNull(body);
+
+        List<String> names = body.stream().map(PersonDTO::toPerson).map(Person::getName).collect(Collectors.toList());
+        assertThat(names, containsInAnyOrder("Natalia"));
+    }
+
+    private String getURL() {
+        return "http://localhost:" + port + "/people";
+    }
 
 }
